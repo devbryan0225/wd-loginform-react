f41b93f0d1242c531acb005ce729ad2a4cf94d54
devbryan0225 <devbryan0225@users.noreply.github.com>
2021-08-08 22:47:32 +0800

    Draft login design.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div >
-      
+    <div className="mx-10" >
+      {/* https://serverless-stack.com/chapters/create-a-login-page.html */}
+      {/* https://dev.to/diraskreact/create-simple-login-form-in-react-227b */}
+      {/* https://tailwindcomponents.com/component/login-form */}
+      <div className="flex flex-col items-center justify-center">
+        <div className="mb-4">
+          <label className="block font-bold text-md text-gray-700"  htmlFor="email">Email</label>
+          <input className="border-2 rounded-md py-2 px-4" id="email" />
+        </div>
+
+        <div className="mb-4">
+          <label className="block font-bold text-md text-gray-700" htmlFor="password">Password</label>
+          <input className="border-2 rounded-md py-2 px-4" id="password" />
+        </div>
+        <div className="">
+          <button className="px-4 py-2 rounded-lg bg-blue-400 hover:bg-blue-500  text-white" type="submit">Login</button>
+        </div>
+      </div>
+
     </div>
   );
 }
